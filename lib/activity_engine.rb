d9c4deadc50ae01d393c9a0ce9d5b1cad076f0b4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-07-24 10:29:24 -0400

    Adding cache sweeper interface
    @WIP, @TODO - Need to add tests and clean up API.

--- lib/activity_engine.rb
+++ lib/activity_engine.rb
@@ -1,9 +1,48 @@
-require "activity_engine/engine"
-require 'activity_engine/context_builder'
-require 'activity_engine/activity_builder'
-
+require 'activity_engine/engine'
 module ActivityEngine
   module_function
+  def register_models(*models)
+    models.flatten!
+    models.collect! { |model| model.respond_to?(:to_sym) ? model.to_s.camelize.constantize : model }
+    models.each {|model|
+      model.send(:include, ActiveModel::Observing) unless model.included_modules.include?(ActiveModel::Observing)
+    }
+    ActivitySweeper.send(:observe, models)
+  end
+
+  def register_controller(controller_name, actions)
+    controller_name.constantize.send(:cache_sweeper, ActivityEngine::ActivitySweeper, only: actions)
+  end
+
+  class ActivitySweeper < ActionController::Caching::Sweeper
+    attr_accessor :records_processed
+
+    def after(controller)
+      self.controller = controller
+      self.records_processed = []
+      callback(:after)
+      self.records_processed = []
+      self.controller = nil
+      true
+    end
+
+    def after_show(record)
+      require 'debugger'; debugger; true
+    end
+
+    def after_save(record)
+      if ! self.records_processed.include?(record)
+        self.records_processed << record
+        ActivityEngine::Activity.new.tap {|activity|
+          activity.subject = record
+          activity.user = current_user
+          activity.activity_type = "#{controller_name}##{action_name}"
+        }.save!
+      end
+      true
+    end
+  end
+
 
   def register(class_name, method_name, activity_receiver = ActivityEngine::Activity, &config_block)
     context_builder = ContextBuilder.new(class_name, method_name)

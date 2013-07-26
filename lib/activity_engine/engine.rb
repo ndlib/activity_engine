4228d693e6b76590eccc087f52a37c49681bab58
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-07-26 15:11:21 -0400

    Adding ActivityEngine::EntryBuilder
    In moving towards the builder I'm envisioning command plugin options.
    Also capturing any configurations for thos registries and evaluating
    them when the behavior is invoked.

--- lib/activity_engine/engine.rb
+++ lib/activity_engine/engine.rb
@@ -6,7 +6,6 @@ module ActivityEngine
       g.test_framework :rspec, :fixture => false
     end
 
-
     initializer "activity_engine.initialize", after: :load_config_initializers do
       require 'activity_engine/activity_sweeper'
     end

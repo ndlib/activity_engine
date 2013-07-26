dcaa8005805617ffdfd2be132dd3af3c720233e2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-07-26 08:56:18 -0400

    Removing spec/dummy; This will be regenerated
    Instead of relying on a dummy application that is prone to be out of
    sync with reality, I am working on regenerating the spec/dummy.
    
    It sounds as though that is the Rails 4 behavior, however, with Rails 3
    still kicking around, I'm planning on using the scripts found here
    https://gist.github.com/stas/4131823

--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '88eb19c49c0c998248d3b5c8c66c0f24a65d9cb8fe759790b73b28479b9fc83a5591263bffaab58cb6a75e796e844864e041f41e919943d6c96a2e67b2568009'
--- spec/dummy/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-
-  mount ActivityEngine::Engine => "/activity_engine"
-end

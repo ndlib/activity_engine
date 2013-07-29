bdab8d1144f5ffebd7db969e9b184f0e9fec7676
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-07-29 15:12:38 -0400

    Tidying up RegisterCall
    Providing means of overwriting collaborating classes

--- lib/activity_engine/entry_builder/commands/register_call.rb
+++ lib/activity_engine/entry_builder/commands/register_call.rb
@@ -4,6 +4,8 @@ module ActivityEngine
   class EntryBuilder
     module Commands
       class RegisterCall < ActivityEngine::EntryBuilder::Command
+        attr_reader :method_name
+        attr_reader :configuration_proc
         def initialize(builder, method_name, &configuration_proc)
           super(builder)
           @method_name = method_name
@@ -12,8 +14,23 @@ module ActivityEngine
 
         def call
           models.each do |model|
-            engine.register_call(model, @method_name, &@configuration_proc)
+            context_wrapper = context_builder.call(model, method_name)
+            activity = activity_builder.call(activity_receiver, configuration_proc)
+            context_wrapper.wrap!(activity)
           end
+        end
+
+        attr_writer :activity_receiver, :context_builder, :activity_builder
+        def activity_receiver
+          @activity_receiver ||= ActivityEngine::Activity
+        end
+
+        def context_builder
+          @context_builder ||= ContextBuilder.method(:new)
+        end
+
+        def activity_builder
+          @activity_builder ||= ActivityBuilder.method(:new)
         end
       end
     end
